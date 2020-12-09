d954ead2ba924bcf0cb7e5058ad3d1a027cd8f24
OMOREAU <olivia.moreau@ynov.com>
2020-12-09 22:07:59 +0100

    update

--- boucles.sh
+++ boucles.sh
@@ -1,27 +1,36 @@
+nbr=0
+while [ $nbr -ne 18 ] ; do
+
+    ((nbr=nbr+1))
+    echo $nbr
+done
+
+
+
 
 j=2
 
 for (( i=1; i<=5; i++ )); do
-    echo "boucle for $i"
+    echo boucle for $i
 done
 
 while [ $j -lt 5 ]; do
     j=`expr $j + 1`
-    echo "Coucou $j"
+    echo $j
 done
 
 #while [ -z $reponse ] || [ $reponse != 'Coucou' ]; do
  #   read -p "dites Coucou : " reponse
 #done
 
-while 
-    echo "Chaîne ? "
-    read nom
-    [ -z $nom ]; do
-    echo "ERREUR : pas de saisie"
-done
-echo "Vous avez saisi : $nom"
+#while 
+#    echo "Chaîne ? "
+#    read nom
+#    [ -z $nom ]; do
+#    echo "ERREUR : pas de saisie"
+#done
+#echo "Vous avez saisi : $nom"
 
-cat git.sh | while read line; do
-    echo $line
-done
+#cat git.sh | while read line; do
+#    echo $line
+#done
